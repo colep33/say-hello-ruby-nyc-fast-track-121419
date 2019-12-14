3f64a090f020d422a5e33a6eb7ff87c403a721d6
Cole Arthur Parham <coleparham@gmail.com>
2019-12-14 17:17:11 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -2,9 +2,10 @@ def say_hello(name)
   puts "Hello #{name}!"
 end
 
-def say_hello
+def say_hello_default
   puts "Hello Ruby Programmer!"
 end
 
-puts say_hello
+puts say_hello_default
+
 puts say_hello("cole")
